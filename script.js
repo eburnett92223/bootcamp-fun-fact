eba4bc6ba95a6420a1b8ec1deceea537a8b4cddc
eburnett92223 <eburnett92223@gmail.com>
2025-06-05 18:51:39 -0700

    Updated file

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const facts = [
   "Octopuses have three hearts.",
   "Honey never spoils.",
   "A group of flamingos is called a flamboyance.",
-  "The Eiffel Tower can grow taller in summer."
+  "The Eiffel Tower can grow taller in summer.",
+  "Rainbows are an effect from total internal reflection."
 ];
 
 function showFact() {
